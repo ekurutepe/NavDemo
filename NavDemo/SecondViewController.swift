059a54806037b6010784ee5f227fc2de8db06bdc
Engin Kurutepe <engin@kurutepe.com>
2015-09-11 15:59:04 +0200

    Initial Commit

---
+++ NavDemo/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  NavDemo
+//
+//  Created by Engin Kurutepe on 11/09/15.
+//  Copyright (c) 2015 Automatic Labs. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
